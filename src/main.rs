fd94e72ee329078581fe418c34b35a8e43d49390
Layton Burchell <laytonb@hotmail.co.uk>
2023-11-16 15:49:29 +0000

    add bug log

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// BUG taking a piece that can capture ends turn early, which causes a crash
+
 use kar_checkers_minmax::checkers::Checkers;
 use kar_checkers_minmax::game_manager::GameManager;
 use kar_checkers_minmax::player::Player;
